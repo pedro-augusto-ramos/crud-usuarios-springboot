c826c5a2a8666fc25b28e85f89c2d7a5266a05d9
pedro-augusto-ramos <trabalhopedroaugustodev@gmail.com>
2025-08-04 12:03:40 -0300

    feat: criado o model Usuario'

---
+++ src/main/java/com/pedroramos/crudusuarios/model/Usuario.java
@@ -0,0 +1,26 @@
+package com.pedroramos.crudusuarios.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private Integer idade;
+
+}
