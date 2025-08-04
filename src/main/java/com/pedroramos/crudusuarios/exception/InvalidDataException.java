b32b389bc04d9582789d1d80e761bcbd5d65715a
pedro-augusto-ramos <trabalhopedroaugustodev@gmail.com>
2025-08-04 13:45:18 -0300

    feat: criado a exception usuario

---
+++ src/main/java/com/pedroramos/crudusuarios/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package com.pedroramos.crudusuarios.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
