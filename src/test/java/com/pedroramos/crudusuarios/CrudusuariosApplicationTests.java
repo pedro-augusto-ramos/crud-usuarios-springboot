ffb760d4ac4500377a45ce7b90e77c1d51f508b2
pedro-augusto-ramos <trabalhopedroaugustodev@gmail.com>
2025-08-04 11:50:47 -0300

    first commit

---
+++ src/test/java/com/pedroramos/crudusuarios/CrudusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroramos.crudusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
